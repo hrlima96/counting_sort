f0b5620ba0efb0262fdba936b16de533fcf705e6
Hugo Rodrigues de Lima <hrlima96@gmail.com>
2015-03-17 13:51:43 -0300

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,17 +1,17 @@
 def merge(lista_1,lista_2):
-	if lista_1 == []:
+    if lista_1 == []:
     	return lista_2
-  	elif lista_2 == []:
+    elif lista_2 == []:
        	return lista_1
-	else:
+    else:
       	if lista_1[0] < lista_2[0]:
-          	return [lista_1[0]] + merge(lista_1[1:], lista_2)
+            return [lista_1[0]] + merge(lista_1[1:], lista_2)
       	else:
-          	return [lista_2[0]] + merge(lista_1, lista_2[1:])
+            return [lista_2[0]] + merge(lista_1, lista_2[1:])
  
 def mergesort(lista):
-  	if len(lista) <= 1:
+    if len(lista) <= 1:
      	return lista
-  	else:
+    else:
     	meio = len(lista) // 2
       	return merge(mergesort(lista[:meio]), mergesort(lista[meio:]))
